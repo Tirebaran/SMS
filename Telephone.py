db7a4bf6381d793ca952948ece44254e28a689c2
Tirebaran <gerart8@mail.ru>
2025-03-13 10:04:35 +0300

    Button fix
    Fixed the button moving out problem by replacing label with enter. Added delete button to delete all entered data, and send button sends entered data to the console

--- Telephone.py
+++ Telephone.py
@@ -1,19 +1,36 @@
 from tkinter import *
 from tkinter import ttk
-def clicked(number):
-    current_text = result.cget("text")  
-    new_text = current_text + str(number)  
-    result.config(text=new_text)  
 
+def clicked(text):
+    current_text = result.get()
+    new_text = current_text + str(text)
+    result.config(state='normal')
+    result.delete(0, END)
+    result.insert(0, new_text)
+    result.config(state='readonly')
+
+def delete():
+    result.config(state='normal')
+    result.delete(0, END)
+    result.config(state='readonly')
+
+def send():
+    text = result.get()
+    print(text)
+    
 root = Tk()
-root.title("METANIT.COM")
+root.title("NOKIA 3310")
 root.geometry("300x400")
- 
+root.resizable(False, False)
+root.iconbitmap(default="Telephone_icon.ico")
+root.attributes("-topmost",True)
+
+
 for c in range(4): root.columnconfigure(index=c, weight=3)
 for r in range(4): root.rowconfigure(index=r, weight=3)
  
-result = ttk.Label()
-result.grid(row=0, column=1, columnspan=3, ipadx=70, ipady=6, padx=5, pady=5)
+result = ttk.Entry(justify='right', state='readonly')
+result.grid(row=0, column=0, columnspan=3, ipadx=70, ipady=6, padx=5, pady=5)
  
 btn1 = ttk.Button(text="    1\nQUIT", command=lambda:clicked(1))
 btn1.grid(row=1, column=0, ipadx=6, ipady=6, padx=5, pady=5)
@@ -42,7 +59,13 @@ btn8.grid(row=3, column=1, ipadx=6,  ipady=6, padx=5, pady=5)
 btn9 = ttk.Button(text="      9\nW X Y Z", command=lambda:clicked(9))
 btn9.grid(row=3, column=2, ipadx=6,  ipady=6, padx=5, pady=5)
 
+btnsend = ttk.Button(text="send", command=lambda: send())
+btnsend.grid(row=4, column=0, ipadx=6,  ipady=6, padx=5, pady=5)
+
 btn0 = ttk.Button(text="   0\n_____", command=lambda:clicked(0))
 btn0.grid(row=4, column=1, ipadx=6,  ipady=6, padx=5, pady=5)
+
+btndel = ttk.Button(text="delete", command=lambda: delete())
+btndel.grid(row=4, column=2, ipadx=6,  ipady=6, padx=5, pady=5)
 
 root.mainloop()
